fb0d2de7358780b518fb633c8b5c0e2400f6dea8
Nico Bermudez <bermnicolas@gmail.com>
2019-03-11 11:39:57 -0400

    Edit Goal functionality

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -10,16 +10,12 @@ class GoalsController < ApplicationController
     erb :'goals/home'
   end
 
+  get '/feed' do
+    erb :'goals/feed'
+  end
+
   get '/create-goal' do
     erb :"goals/create-goal"
-  end
-
-  get '/edit-goal' do
-    erb :'/goals/edit-goal'
-  end
-
-  get '/delete-goal' do
-    erb :'/goals/delete-goal'
   end
 
   post '/create-goal' do
@@ -36,12 +32,44 @@ class GoalsController < ApplicationController
     end
   end
 
-  post '/delete-goal' do
-    # goal.delete 
+  get '/goals/:id/edit' do
+    if logged_in?
+      @goal = Goal.find_by_id(params[:id])
+      if @goal && @goal.user == current_user
+        erb :"goals/edit-goal"
+      else
+        redirect to '/home'
+      end
+    else
+      redirect to '/'
+    end
   end
 
-  get '/feed' do
-    erb :'goals/feed'
+  patch '/goals/:id' do
+    @goal = Goal.find_by_id(params[:goal][:id])
+    if logged_in?
+      if params[:goal][:title] == "" || params[:goal][:description] == "" || params[:goal][:action_plan] == "" || params[:goal][:deadline] == ""
+        flash[:message] = "Entry not saved. Please do not leave any input blank. Try again."
+        redirect to '/goals/:id/edit'
+      else
+        @goal = Goal.find_by_id(params[:id])
+        if @goal && @goal.user == current_user
+          if @goal.update(title: params[:goal][:title], description: params[:goal][:description], action_plan: params[:goal][:action_plan], deadline: params[:goal][:deadline])
+            redirect to '/home'
+          else
+            flash[:message] == "You did not properly edit tweet, please try again"
+            redirect to '/goals/:id/edit'
+          end
+        else
+          redirect to 'home'
+        end
+        redirect to '/'
+      end
+    end
+  end
+
+  delete '/goal/:id/delete' do
+    # goal.delete
   end
 
 end
