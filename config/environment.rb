e689dea8d80b74b509eb39eb166a94a9042b2213
Nico Bermudez <bermnicolas@gmail.com>
2019-06-12 14:35:34 -0400

    Bug fix

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ configure :production do
  db = "postgres://ikpimkzqqprigx:e1e1ef812be078a498531b1b9369a8decd9e66aff358a407c4add245b2116b34@ec2-54-197-239-115.compute-1.amazonaws.com:5432/d2ck2qmokrbn69"
 
  ActiveRecord::Base.establish_connection(
-   :adapter  => 'postgres',
+   :adapter  => 'postgresql',
    :host     => 'ec2-54-197-239-115.compute-1.amazonaws.com',
    :username => 'ikpimkzqqprigx',
    :password => 'e1e1ef812be078a498531b1b9369a8decd9e66aff358a407c4add245b2116b34',
